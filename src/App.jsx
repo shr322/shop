482a5739fcc75af1b058b57d1bf51fe5f54cf7a5
심형래 <shr322@naver.com>
2025-06-27 09:19:06 +0900

    회사컴

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
 
+/* 추가 */
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, Container, Nav, Navbar } from 'react-bootstrap';
+import styleBg from './bg.png';
+import data1 from './data';
+/* import export할때 여러개 불러오는 경우에는 변수명을 js파일과 동일하게 해야한다. */
+import {a,b} from './import_export_test';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [shoes, setShoes] = useState(data1)
 
   return (
     <>
@@ -24,6 +29,27 @@ function App() {
         </Navbar>
 
         <div className="main-bg"></div>
+        {/* <div className="main-bg" style={{background: `url(${styleBg})`}}></div> */}
+
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4">
+              <img src="/shoes1.jpg" width="80%" />
+              <h4>상품명</h4>
+              <p>상품정보</p>
+            </div>
+            <div className="col-md-4">
+              <img src="/shoes2.jpg" width="80%" />
+              <h4>상품명</h4>
+              <p>상품정보</p>
+            </div>
+            <div className="col-md-4">
+              <img src="/shoes3.jpg" width="80%" />
+              <h4>상품명</h4>
+              <p>상품정보</p>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   )
