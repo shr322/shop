03a465635ce2f18bacdf54e6413a4c7267731ec6
Hyung Rae Sim <shr322@naver.com>
2025-07-03 00:08:47 +0900

    useEffect input입력값 체크

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -27,32 +27,64 @@ import { useParams } from "react-router-dom";
 
 function Detail(props){
 
-  
-
   let [count, setCount] = useState(0);
   const {id} = useParams();
   let 찾는상품 = props.shoes.find((a,b)=>{ return a.id == id })
   const [alert, setAlert] = useState(true)
+  const [inp, setinp] = useState('');
 
-
+  /**
+   * useEffect는
+   * mount,update시 실행됨
+   * 
+   * 1. 이러면 재렌더링마다 코드를 실행가능합니다.
+   * useEffect(()=>{ 실행할코드 })
+   * 
+   * 2. 이러면 컴포넌트 mount시 (로드시) 1회만 실행가능합니다.
+   * useEffect(()=>{ 실행할코드 }, [])
+   * 
+   * 3. 이러면 useEffect 안의 코드 실행 전에 항상 실행됩니다. 
+   * useEffect(()=>{ return ()=>{ 실행할코드 }})
+   * 
+   * 4. 이러면 컴포넌트 unmount시 1회 실행됩니다.
+   * useEffect(()=>{return ()=>{실행할코드}}, [])
+   * 
+   * 5. 이러면 state1이 변경될 때만 실행됩니다. 
+   * useEffect(()=>{실행할코드}, [state1])
+   * */
   useEffect(()=>{
-    setTimeout(() => {
+    let a = setTimeout(() => {
       setAlert(false)
     }, 2000);
+    
+    return()=>{
+      clearTimeout(a)
+    }
   })
+
+  useEffect(()=>{
+    if(isNaN(inp)){
+      console.log('숫자가 아닙니다')
+    }
+  },[inp])
+
   
   return (
     <div className="container">
-      <button onClick={()=>{
+      {count}<button onClick={()=>{
         setCount(count+1);
       }}>버튼</button>
 
       { alert ? <div className="alert alert-warning">2초이내 구매시 할인</div> : null}
 
+
       <div className="row">
         <div className="col-md-6">
           <img src={`https://codingapple1.github.io/shop/shoes${parseInt(찾는상품.id) + 1}.jpg`} width="100%" />
         </div>
+        <input type="text" onChange={(e)=>{
+          setinp(e.target.value)
+        }}/>
         <div className="col-md-6">
           <h4 className="pt-5">{찾는상품.title}</h4>
           <p>{찾는상품.content}</p>
