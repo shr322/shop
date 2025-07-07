44829ca1db17bfb7d5c1c4ee3d80aab9f66ea54a
심형래 <shr322@naver.com>
2025-07-07 18:07:50 +0900

    useEffect, fetch, 리펙토링 등 shop강의 들은곳까지 스스로 해봄

--- src/App_내가해봄.jsx
+++ src/App_내가해봄.jsx
@@ -1,16 +1,20 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Button, Navbar, Container, Row, Col, Nav, Card } from "react-bootstrap";
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import { Routes, Route, useNavigate, Outlet, useMatch } from 'react-router-dom';
 import data from './data';
 import Detail from './pages/Detail_내가해봄';
+import axios from "axios";
 
 function App() {
   const [shoes, setShoes] = useState(data);
   const navigate = useNavigate();
+  const match = useMatch('/detail')
+
+  const [loadOfOff, setLoadOfOff] = useState(true)
 
   return (
     <>
@@ -20,7 +24,7 @@ function App() {
             <Navbar.Brand><Nav.Link onClick={()=>{navigate('/')}}>Home</Nav.Link></Navbar.Brand>
             <Nav className="me-auto">
               <Nav.Link onClick={()=>{navigate('/detail')}}>상세페이지</Nav.Link>
-              <Nav.Link onClick={()=>{navigate('/2')}}>Pricing</Nav.Link>
+              <Nav.Link onClick={()=>{navigate('/about')}}>about</Nav.Link>
             </Nav>
           </Container>
         </Navbar>
@@ -28,45 +32,42 @@ function App() {
         <div className="main-bg"></div>
 
         <Routes>
+          {/* 메인 */}
           <Route path="/" element={
             <>
               <div>메인입니다.</div>
             </>
           }/>
+          {/* 메인 */}
+
+          {/* 상세페이지 목록 */}
           <Route path="/detail" element={
             <>
-              <button onClick={()=>{
-                const copy = [...shoes];
-                copy.sort((a,b)=>{
-                  if(a.title > b.title) return 1
-                  if(a.title < b.title) return -1
-                })
-                setShoes(copy)
-              }}>정렬</button>
-              <Container>
-                <Row>
-                  {shoes.map((item)=>{
-                    return <CardComponent item={item}/>
-                  })}
-                  
-                </Row>
-              </Container>
+              <DetailList loadOfOff={loadOfOff} setLoadOfOff={setLoadOfOff} navigate={navigate} shoes={shoes} setShoes={setShoes}/>
+              {loadOfOff ? <div className="loading">로딩중</div> : null}
             </>
-          } />
-          <Route path="/2" element={<Detail></Detail>} />
+          }>
+          </Route>
+          {/* 상세페이지 목록 */}
+
+          <Route path="/detail/:id" element={<Detail shoes={shoes}/>}></Route>
+
+          <Route path="/about" element={<div>about</div>}></Route>
+
           <Route path="*" element={<div>없음</div>} />
         </Routes>
 
 
         
       </div>
+
+      
     </>
   );
 }
 
 
 function CardComponent(props){
-  console.log(props.item.id)
   return (
     <>
       <Col xs={6} md={4}>
@@ -76,10 +77,67 @@ function CardComponent(props){
             <Card.Title>{props.item.title}</Card.Title>
             <Card.Text>{props.item.content}</Card.Text>
             <Card.Text>{props.item.price}원</Card.Text>
-            <Button variant="primary">Go somewhere</Button>
+            <Button variant="primary" onClick={()=>{props.navigate(`/detail/${props.item.id}`)}}>Go somewhere</Button>
           </Card.Body>
         </Card>
       </Col>
+    </>
+  )
+}
+
+function DetailList(props){
+  const [count, setCount] = useState(0);
+  
+  useEffect(()=>{
+    props.setLoadOfOff(false)  
+  }, [])
+
+  return (
+    <>
+      <button onClick={()=>{
+        const copy = [...props.shoes];
+        copy.sort((a,b)=>{
+          if(a.title > b.title) return 1
+          if(a.title < b.title) return -1
+        })
+        props.setShoes(copy)
+      }}>정렬</button>
+      <button onClick={()=>{
+        
+        async function fetchData(url) {
+          try{
+            props.setLoadOfOff(true)
+            const result = await fetch(url);
+            const data = await result.json();
+            const copy = [...props.shoes, ...data];
+            props.setShoes(copy)
+            setCount(count + 1);
+          } catch(err){
+            console.log(err)
+          } finally {
+            console.log('호출완료')
+              props.setLoadOfOff(false)  
+          }
+        }
+
+        if(count === 0){
+          fetchData('https://codingapple1.github.io/shop/data2.json')
+          console.log(count)
+        } else if(count === 1) {
+          fetchData('https://codingapple1.github.io/shop/data3.json')
+          console.log(count)
+        } else {
+          props.setLoadOfOff(false)
+          alert('불러올 상품이 없음')
+        }
+      }}>더보기</button>
+      <Container>
+        <Row>
+          {props.shoes.map((item,i)=>{
+            return <CardComponent key={i} navigate={props.navigate} item={item}/>
+          })}
+        </Row>
+      </Container>
     </>
   )
 }
--- src/pages/Detail_내가해봄.jsx
+++ src/pages/Detail_내가해봄.jsx
@@ -1,21 +1,43 @@
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 function Detail(props){
+  const {id} = useParams();
+  const [alertOnOff, setAlertOnOff] = useState(true)
+
+  const findProduct = props.shoes.find((a)=>{
+    return a.id == id;
+  })
+
+
+  useEffect(()=>{
+    let a = setTimeout(() => {
+      setAlertOnOff(false)
+    }, 5000);
+
+    return ()=>{
+      clearTimeout(a)
+    }
+  }, [])
+
   return (
-    <div class="card mb-3" >
-      <div class="row g-0">
-        <div class="col-md-4">
-          <img src="..." class="img-fluid rounded-start"/>
-        </div>
-        <div class="col-md-8">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+    <>
+      {alertOnOff ? <div className="alert alert-warning">5초이내 구매시 할인</div> : null}
+      <div className="card mb-3" >
+        <div className="row g-0">
+          <div className="col-md-4">
+            <img src={`https://codingapple1.github.io/shop/shoes${parseInt(findProduct.id) + 1}.jpg`} className="img-fluid rounded-start"/>
+          </div>
+          <div className="col-md-8">
+            <div className="card-body">
+              <h5 className="card-title">{findProduct.title}</h5>
+              <p className="card-text">{findProduct.content}</p>
+              <p className="card-text"><small className="text-body-secondary">{findProduct.price}</small></p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 export default Detail;
